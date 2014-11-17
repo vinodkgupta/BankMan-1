3e6f5c52748e129ddb6135074a9d65df55c1c4c6
vinodkgupta <vinodkgupta93@gmail.com>
2014-11-18 00:00:41 +0530

    update server_connect.inc.php
    commenting

--- server_connect.inc.php
+++ server_connect.inc.php
@@ -1,6 +1,10 @@
+
 <?php
-if(!mysql_connect('localhost','samya','password') || !mysql_select_db('BankMan'))
-{	$error='Cant connect'; 
-	die($error);
+                                                                                  // php code begins
+
+if(!mysql_connect('localhost','root','') || !mysql_select_db('BankMan_heavy1'))  // connect to mysql server and also required database
+{	$error='Cant connect';                                 
+	die($error);                                                             // use die to abort and give error message 
+                                                                                 
 }
 ?>
